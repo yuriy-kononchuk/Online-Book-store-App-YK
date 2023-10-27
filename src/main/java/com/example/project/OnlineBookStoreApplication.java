fbd292bc255a00896b9cea7faa565064e8f34d10
Yuriy Kononchuk <y_kononchuk@ukr.net>
2023-10-27 16:49:28 +0300

    created structure for project book-store

---
+++ src/main/java/com/example/project/OnlineBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookStoreApplication.class, args);
+	}
+
+}
