0e7c89a8986731aad46d5cee2afaa80a32f7bee4
Yuriy Kononchuk <y_kononchuk@ukr.net>
2023-11-08 20:52:10 +0200

    fixed annotations for Book class

--- src/main/java/com/example/project/model/Book.java
+++ src/main/java/com/example/project/model/Book.java
@@ -14,21 +14,24 @@ import org.hibernate.annotations.Where;
 
 @Entity
 @Data
-@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
-@Where(clause = "is_deleted=false")
+@SQLDelete(sql = "UPDATE books SET is_deleted = TRUE WHERE id = ?")
+@Where(clause = "is_deleted = FALSE")
 @Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotNull
+    @Column(nullable = false)
     private String title;
     @NotNull
+    @Column(nullable = false)
     private String author;
     @NotNull
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String isbn;
     @NotNull
+    @Column(nullable = false)
     private BigDecimal price;
     private String description;
     @Column(name = "cover_image")
